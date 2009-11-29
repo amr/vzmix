664fd2eff737612e9c57323a1efb6747f7df872e
Amr Mostafa <amr.mostafa@gmail.com>
2009-11-29 22:07:29 +0200

    Refactor UBC's value into abstract class UBCValue.
    This paves the way for subclasses of UBCValue which are more specific to a special value and know how to validate and represent themselves better for the UBC parameter value they represent. Other improvements:
      * Fixed the implementation of arithmetic operations which was confused of
        the mutability of the objects
      * The above in turn, makes the line clear of what's mutable and what's not
      * Added some docstrings, hopefully more will be added with more useful info

--- vzmix.py
+++ vzmix.py
@@ -18,28 +18,19 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-# Represents a User Beancounter
-# See: http://wiki.openvz.org/UBC
-class UBC:
-    name = None
-    barrier = None
-    limit = None
+class UBCValue:
+    """Represents a User Beancounter value"""
+    # Internal value
+    value = None
 
-    # Maximum number that can be represented by an 8-byte integer
-    # This is the largest number that OpenVZ can accept AFAIK
+    # The maximum value allowed, the default is an 8-byte integer. This is the
+    # largest number that OpenVZ can accept AFAIK
     cap = 9223372036854775807
 
-    def __init__(self, name=None, barrier=None, limit=None):
-        self.name = name
-        self.barrier = int(barrier)
-        if limit is not None:
-            self.limit = int(limit)
-        
-    def __str__(self):
-        if self.limit is not None:
-            return '%s="%d:%d"' % (self.name, self.barrier, self.limit)
-        else:
-            return '%s="%d"' % (self.name, self.barrier)
+    def __init__(self, value):
+        if not isinstance(value, (int, long)):
+            value = int(value)
+        self.value = value
 
     def ensureCap(self, value):
         if value > self.cap:
@@ -47,25 +38,80 @@ class UBC:
         return value
 
     def __mul__(self, factor):
-        self.barrier = self.ensureCap(int(self.barrier * factor))
-        if self.limit is not None:
-            self.limit = self.ensureCap(int(self.limit * factor))
+        factor = objectToNumber(factor)
+        return UBCValue(self.ensureCap(self.value * factor))
 
     def __add__(self, other):
-        self.barrier = self.ensureCap(self.barrier + other.barrier)
-        if self.limit is not None and other.limit is not None:
-            self.limit = self.ensureCap(self.limit + other.limit)
-        return self
+        other = objectToNumber(other)
+        return UBCValue(self.ensureCap(self.value + other))
 
     def __sub__(self, other):
-        self.barrier = self.ensureCap(self.barrier - other.barrier)
+        other = objectToNumber(other)
+        return UBCValue(self.ensureCap(self.value - other))
+
+    def __int__(self):
+        """Converts self to int"""
+        return int(self.value)
+    __trunc__ = __int__
+
+    def __float__(self):
+        """Converts self to float"""
+        return float(self.value)
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return str(self.value)
+
+class UBC:
+    """Represents a User Beancounter
+
+    See: http://wiki.openvz.org/UBC
+    """
+    name = None
+    barrier = None
+    limit = None
+
+    def __init__(self, name=None, barrier=None, limit=None):
+        self.name = name
+        self.barrier = UBCValue(barrier)
+        if limit is not None:
+            self.limit = UBCValue(limit)
+        
+    def __str__(self):
+        if self.limit is not None:
+            return '%s="%d:%d"' % (self.name, self.barrier, self.limit)
+        else:
+            return '%s="%d"' % (self.name, self.barrier)
+
+    def multiply(self, other):
+        """Multiplies self by given object"""
+        self.barrier *= other
+        if self.limit is not None:
+            self.limit *= other
+
+    def add(self, other):
+        """Adds given object to self"""
+        if not isinstance(other, UBC):
+            raise TypeError("Cannot add objects of type %s to UBC" % type(other))
+
+        self.barrier += other.barrier
         if self.limit is not None and other.limit is not None:
-            self.limit = self.ensureCap(self.limit - other.limit)
-        return self
+            self.limit += other.limit
+
+    def substract(self, other):
+        """Substracts given object from self"""
+        if not isinstance(other, UBC):
+            raise TypeError("Cannot substract objects of type %s from UBC" % type(other))
+
+        self.barrier -= other.barrier
+        if self.limit is not None and other.limit is not None:
+            self.limit -= other.limit
 
 
-# Represents a Container Configuration file
 class CTConfig:
+    """Represents a Container Configuration file"""
     # Internal storage of UBCs and other file contents
     data = []
 
@@ -77,7 +123,7 @@ class CTConfig:
     # Populates CTConfig from given container configuration file
     def fromFile(self, filename):
         f = open(filename, 'r')
-        lines = f.readlines();
+        lines = f.readlines()
         f.close()
 
         return self.fromLines(lines)
@@ -123,27 +169,34 @@ class CTConfig:
                 if ubc.name == name:
                     return ubc
 
-    def __mul__(self, factor):
+    def multiply(self, other):
         for ubc in self.data:
             if isinstance(ubc, UBC):
-                ubc *= factor
-        return self
+                ubc.multiply(other)
 
-    def __add__(self, other):
+    def add(self, other):
         for oubc in other.data:
             if isinstance(oubc, UBC):
                 ubc = self.getUBC(oubc.name)
                 if ubc is not None:
-                    ubc += oubc
-        return self
+                    ubc.add(oubc)
 
-    def __sub__(self, other):
+    def substract(self, other):
         for oubc in other.data:
             if isinstance(oubc, UBC):
                 ubc = self.getUBC(oubc.name)
                 if ubc is not None:
-                    ubc -= oubc
-        return self
+                    ubc.substract(oubc)
+
+
+# Helper function to convert a given object to a number
+def objectToNumber(obj):
+    if isinstance(obj, (float, int, long)):
+        return obj
+    elif isinstance(obj, UBCValue):
+        return int(obj.value)
+    else:
+        raise TypeError("Cannot convert %s to a number" % (obj))
 
 
 def main():
@@ -177,23 +230,22 @@ def main():
             if options.multiply <= 0:
                 cli.error("Invalid multiplication factor %s" % str(options.multiply))
         
-            c *= options.multiply
+            c.multiply(options.multiply)
 
-        # Addition
+        # Add
         if options.add is not None:
             for f in options.add:
-                c += CTConfig(f)
+                c.add(CTConfig(f))
 
         # Substract
         if options.substract is not None:
             for f in options.substract:
-                c -= CTConfig(f)
+                c.substract(CTConfig(f))
 
         c.toString()
     except Exception, e:
         if options.debug:
             raise
-
         else:
             cli.print_usage()
             cli.exit(2, "%s: %s\n" % (cli.get_prog_name(), e))
